59f182cb02f76b26fe9ef8824aedc935445c423b
DarkZant <etiennemb8@gmail.com>
2023-12-04 09:26:17 -0500

    Initial commit

---
+++ src/main/java/unishop/Evaluation.java
@@ -0,0 +1,19 @@
+package unishop;
+
+public class Evaluation {
+    final String nomAcheteur;
+    final int note;
+    private final String commentaire;
+
+    public Evaluation(String nomAcheteur, int note, String commentaire) {
+        this.nomAcheteur = nomAcheteur;
+        this.note = note;
+        this.commentaire = commentaire;
+    }
+    public String getSaveFormat() {
+        return nomAcheteur + "," + note + "," + commentaire;
+    }
+    public String getDisplayFormat() {
+        return "Acheteur: " + nomAcheteur + "\nNote sur 5: " + note + "\n" + commentaire;
+    }
+}
